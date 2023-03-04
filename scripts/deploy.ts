40b2c7a7a87e2084d54d53e2a3158c7b1c44fc42
Evans007 <ebounce500@gmail.com>
2023-03-04 14:26:35 +0100

    fixed merkle system, contract interaction is not completed

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,124 +1,96 @@
+
 import { ethers } from "hardhat";
-import { keccak256, Bytes } from "ethers/lib/utils";
 import { MerkleTree } from "merkletreejs";
-
-import * as fs from "fs";
+import { keccak256 } from "ethers/lib/utils";
+import { parse } from "csv-parse";
+import { createReadStream } from "fs";
 import * as path from "path";
-import { parse } from 'csv-parse';
-
-
+import * as fs from "fs";
 
 async function main() {
-  
-  //////////////////////  GET CSV FILE  /////////////////////////
-type AirdropInfo = {
-  address: string;
-  amount: number;
-};
+    
+  // Read the CSV file
+  const file = createReadStream(path.join(__dirname, "winners.csv"), "utf8");
 
-  const csvFilePath = path.resolve(__dirname, '../winners.csv');
+  let fileContents = "";
 
-  const headers = ['address', 'amount'];
-  const outputDirPath = path.join(__dirname, 'output');
-  const outputFilePath = path.join(outputDirPath, 'result.json');
-  const fileContent = fs.readFileSync(csvFilePath, { encoding: 'utf-8' });
-  let leaves: string[] = [];
-  let merkleLeaves = [];
-  let merkleProof = [];
-
-  parse(fileContent, {
-    delimiter: ',',
-    columns: headers,
-  }, (error, result: AirdropInfo[]) => {
-    if (error) {
-      console.error(error);
-    }
-
-     // Create output directory
-  if (!fs.existsSync(outputDirPath)) {
-    fs.mkdirSync(outputDirPath);
-  }
-
-      /// save the json data
-    const outputData = JSON.stringify(result, null, 2);
-    fs.writeFileSync(outputFilePath, outputData);
+  file.on("data", (chunk) => {
+    fileContents += chunk;
   });
 
-  //////////////////////////  MERKLE TREE  ///////////////////////////
-  const filePath = path.join(__dirname, './output');
-  const resultsPath = path.join(filePath, 'result.json');
-  const fileData = fs.readFileSync(resultsPath, 'utf-8');
-  const jsonFile = JSON.parse(fileData);
-  jsonFile.shift();
+  file.on("end", () => {
+    // Parse the CSV file
+    parse(fileContents, {
+      columns: true,
+      skip_empty_lines: true,
+    }, (err, records: Record[]) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
 
-  const list : AirdropInfo[] = jsonFile;
-  for(let i = 0; i < jsonFile.length; i++) {
-    const address = jsonFile[i].address;
-    const amount = jsonFile[i].amount;
+      // Get the leaves for the merkle tree
+      const leaves = records.map((record) => {
+        const address = record["address"];
+        const amount = record["amount"];
+        return keccak256(address + amount);
+      });
 
-    let leaf = ethers.utils.solidityKeccak256(["address","uint256"],[address,amount]);
-    leaves.push(leaf);
-  }
+      // Create the merkle tree
+      const merkleTree = new MerkleTree(leaves, keccak256, { sortPairs: true });
 
-  const leaveList = path.resolve(filePath,'leaves.json');
-  const JSONLeaves = JSON.stringify(leaves);
-  fs.writeFileSync(leaveList,JSONLeaves);
+      // Get the merkle proof for each leaf
+      const proofs = records.map((record) => {
+        const address = record["address"];
+        const amount = record["amount"];
+        const leaf = keccak256(address + amount);
+        const index = leaves.indexOf(leaf);
+        return merkleTree.getProof(leaf, index);
+      });
 
-  const tree = new MerkleTree(leaves, keccak256, { sortPairs: true });
-  const root = tree.getRoot().toString('hex');
-  
-              /// GET PROOF  ///
-  // let leaf1 = "0x1f480e4c9d75a29f01ac782acc15a74ca1c3bfc4914f85f0950ca5e506038669";
-  // let proof = tree.getProof(leaf1);
+      // Save the proofs to a file
+      const proofsJson = JSON.stringify(proofs, null, 2);
+      fs.writeFileSync(path.join(__dirname, "proofs.json"), proofsJson);
 
-  let listXf = JSON.parse(JSONLeaves);
-  for (let i = 0; i < listXf.length; i++) {
-    merkleLeaves.push(listXf[i]);
-  }
-
-console.log(merkleLeaves);
-
-  
-
-
-
-
+      // get merkle root
+      const root = merkleTree.getHexRoot();
+      
+      console.log("Merkle root:", root);
+      // ROOT = 0xc333c8edc6e19be32f2beffc2e45bbc3493341b8dd12ab506fad729f11e53f3b
+    });
+  });
 
 
+    ///////////////////////  DEPLOY CONTRACTS   ////////////////////////////////////
+    // const [owner, account1] = await ethers.getSigners();
+    // const iOwner = owner.address;
 
-  //const verified = tree.verify(proof, leaf1, root);
-  //ROOT for LEAF1 = e1f3dd2e3ff6fb5c04a9347b0ae112d027f12e7ca2aa78c7eede2b8630032e7c
-  
-  ///////////////////////  DEPLOY CONTRACTS   ////////////////////////////////////
-  const [owner, account1] = await ethers.getSigners();
-  // const iOwner = owner.address;
+    // /////////////////////  DEPLOY TMX TOKEN  ///////////////////////
+    // const Token = await ethers.getContractFactory("Token");
+    // const token = await Token.deploy(iOwner,1000000);
+    // await token.deployed();
+    // const TMX = token.address;
+    // console.log(`TMX deployed to: ${TMX}`);
 
-  // /////////////////////  DEPLOY TMX TOKEN  ///////////////////////
-  // const Token = await ethers.getContractFactory("Token");
-  // const token = await Token.deploy(iOwner,1000000);
-  // await token.deployed();
-  // const TMX = token.address;
-  // console.log(`TMX deployed to: ${TMX}`);
-
-  ///////////////////   DEPLOY AIRDROP CONTRACT  /////////////////
-  // const AirdropContract = await ethers.getContractFactory("Airdrop");
-  // const airdrop = await AirdropContract.deploy(
-  //   TMX,
-  //   root,
+    // /////////////////   DEPLOY AIRDROP CONTRACT  /////////////////
+    // const AirdropContract = await ethers.getContractFactory("Airdrop");
+    // const airdrop = await AirdropContract.deploy(
+    //   TMX,
+    //   root,
+      
+    // );
     
-  // );
-  
-  // await airdrop.deployed();
-  // const AirdropContractAddr = airdrop.address;
-  // console.log(AirdropContractAddr);
-  
-}
+    // await airdrop.deployed();
+    // const AirdropContractAddr = airdrop.address;
+    // console.log(AirdropContractAddr);
+    
+}         
 
 
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  // We recommend this pattern to be able to use async/await everywhere
+  // and properly handle errors.
+  main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
